0b36d57ca0eecdf388934e57957abbee2c7983ce
namt <neamaas@ac.sce.ac.il>
2026-05-16 01:30:29 +0300

    Finished pets forms without Excel file

---
+++ source/repos/ClinicVets-main/ClinicVets.Pets/AllPetsForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ClosedXML.Excel;
+
+
+namespace ClinicVets.Pets
+{
+    public partial class AllPetsForm : Form
+    {
+        string filePath = @"C:\Users\ENTER\OneDrive - ac.sce.ac.il\שולחן העבודה\ClinicVets\ClinicVetsData.xlsx";
+
+        public AllPetsForm()
+        {
+            InitializeComponent();
+            LoadPetsFromExcel();
+        }
+
+        private void LoadPetsFromExcel()
+        {
+            dgvPets.Rows.Clear();
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Excel file not found");
+                return;
+            }
+
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var sheet = workbook.Worksheet("Pets");
+
+                foreach (var row in sheet.RangeUsed().RowsUsed())
+                {
+                    if (row.RowNumber() == 1)
+                        continue;
+
+                    dgvPets.Rows.Add(
+                        row.Cell(1).GetValue<string>(),
+                        row.Cell(2).GetValue<string>(),
+                        row.Cell(3).GetValue<string>(),
+                        row.Cell(4).GetValue<string>(),
+                        row.Cell(5).GetDateTime(),
+                        row.Cell(6).GetValue<string>(),
+                        row.Cell(7).GetValue<string>(),
+                        row.Cell(8).GetDateTime()
+                    );
+                }
+            }
+
+            dgvPets.Columns[4].DefaultCellStyle.Format = "dd/MM/yyyy";
+            dgvPets.Columns[7].DefaultCellStyle.Format = "dd/MM/yyyy";
+
+            dgvPets.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ source/repos/ClinicVets-main/ClinicVets.Pets/AnimalTypesForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ClinicVets.Pets
+{
+    partial class AnimalTypesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstTypes = new System.Windows.Forms.ListBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(347, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(526, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Animal Types Management";
+            // 
+            // lstTypes
+            // 
+            this.lstTypes.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.lstTypes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lstTypes.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.lstTypes.FormattingEnabled = true;
+            this.lstTypes.ItemHeight = 26;
+            this.lstTypes.Location = new System.Drawing.Point(438, 155);
+            this.lstTypes.Name = "lstTypes";
+            this.lstTypes.Size = new System.Drawing.Size(388, 264);
+            this.lstTypes.TabIndex = 1;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(591, 469);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(224, 26);
+            this.txtType.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Animal type:";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.Location = new System.Drawing.Point(227, 177);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(144, 60);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add Type";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Location = new System.Drawing.Point(227, 284);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(144, 61);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update Type";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDelete.Location = new System.Drawing.Point(227, 388);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(144, 58);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete Type";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBack.Location = new System.Drawing.Point(871, 521);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(120, 47);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click_1);
+            // 
+            // AnimalTypesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ClinicVets.Pets.Properties.Resources.WhatsApp_Image_2026_05_14_at_16_47_35;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1178, 644);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.lstTypes);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "AnimalTypesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animal Types Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstTypes;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
